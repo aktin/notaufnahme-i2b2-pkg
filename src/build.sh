57d4a977406366043344a88c2bab3d613ca49422
Alexander Kombeiz <alexkombie@gmail.com>
2022-02-01 10:09:50 +0100

    added context of current folder to build and clean, added check for version input

--- src/build.sh
+++ src/build.sh
@@ -7,5 +7,7 @@ VERSION="${1}"
 # Check if variables are empty
 if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
 
-. debian/build.sh "$VERSION"
-. docker/build.sh "$VERSION" "full"
+ROOT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+. $ROOT_DIR/debian/build.sh "$VERSION"
+. $ROOT_DIR/docker/build.sh "$VERSION" "full"
--- src/clean.sh
+++ src/clean.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf debian/build docker/build
+
+ROOT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+rm -rf $ROOT_DIR/debian/build $ROOT_DIR/docker/build
