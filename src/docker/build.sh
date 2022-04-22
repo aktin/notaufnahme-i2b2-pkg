3328ceaa7e9bacaa0e539c75b60d839111a2ac3f
Alexander Kombeiz <alexkombie@gmail.com>
2022-04-22 11:06:51 +0200

    smaller fixes, enhanced cleaning up of docker img

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -4,34 +4,32 @@ set -euo pipefail
 PACKAGE="aktin-notaufnahme-i2b2"
 
 # Required parameter
-VERSION="${1}"
+VERSION="${1:-}"
+if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
 
 # Optional parameter
-FULL="${2}"
-
-# Check if variables are empty
-if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
+FULL="${2:-}"
 
 # Directory this script is located in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 DBUILD="${DIR}/build"
 
-# Cleanup
+# Cleanup old files
 rm -rf "${DIR}/build"
 
-export I2B2IMAGENAMESPACE="$(echo "${PACKAGE}" | awk -F '-' '{print "ghcr.io/"$1"/"$2"-i2b2-"}')"
-export DWHIMAGENAMESPACE="$(echo "${PACKAGE}" | awk -F '-' '{print "ghcr.io/"$1"/"$2"-dwh-"}')"
+# Load content of .env as environment variables 
+export $(cat ${DIR}/.env | xargs)
 
-# Load common linux files
+# Load common linux files (includes versions)
 . "$(dirname "${DIR}")/common/build.sh"
 
 # Prepare wildfly docker
 mkdir -p "${DBUILD}/wildfly"
-sed -e "s/__VWILDFLY__/${VWILDFLY}/g" "${DIR}/wildfly/Dockerfile" >"${DBUILD}/wildfly/Dockerfile"
+sed -e "s/__VWILDFLY__/${VWILDFLY}/g" "${DIR}/wildfly/Dockerfile" > "${DBUILD}/wildfly/Dockerfile"
 cp "${DIR}/wildfly/entrypoint.sh" "${DBUILD}/wildfly/"
 cp "${DRESOURCES}/standalone.xml.patch" "${DBUILD}/wildfly/"
-download_wildfly_i2b2 "/wildfly"
 download_wildfly_jdbc "/wildfly"
+download_wildfly_i2b2 "/wildfly"
 copy_datasource_for_postinstall "/wildfly/ds"
 
 # Prepapare postgresql docker
@@ -49,9 +47,21 @@ config_i2b2_webclient "/httpd/i2b2webclient"
 if [ "${FULL}" = "full" ]; then
 
 	# Clean up old images
-	docker image rm ${I2B2IMAGENAMESPACE}database
-	docker image rm ${I2B2IMAGENAMESPACE}wildfly
-	docker image rm ${I2B2IMAGENAMESPACE}httpd
+	LIST_IMAGES=( ${I2B2IMAGENAMESPACE}database ${I2B2IMAGENAMESPACE}wildfly ${I2B2IMAGENAMESPACE}httpd )
+	for IMAGE in ${LIST_IMAGES[*]}; do
+		echo $IMAGE
+		# Stop and remove running container if exists
+  		ID_CONTAINER=$(docker ps -q -f ancestor=${IMAGE}:latest)
+		if [ -n ${ID_CONTAINER} ]; then
+			docker stop ${ID_CONTAINER} || true
+			docker rm ${ID_CONTAINER} || true
+		fi
+		# Remove image
+		ID_IMAGE=$(docker images -q ${IMAGE}:latest)
+		if [ -n ${ID_IMAGE} ]; then
+			docker image rm ${ID_IMAGE} || true
+		fi
+	done
 
 	cwd="$(pwd)"
 	cd "${DIR}"
